f0fdad9f3d66e826c5416c0c9306fe0df61b287d
thembones79 <michal.szumnarski@gmail.com>
2019-03-06 23:30:50 +0100

    added camera handling

--- App.js
+++ App.js
@@ -1,11 +1,81 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  ImageBackground
+} from "react-native";
+import { Camera, Permissions } from "expo";
 
 export default class App extends React.Component {
+  state = {
+    cameraPermission: null
+  };
+
+  componentDidMount() {
+    Permissions.askAsync(Permissions.CAMERA).then(({ status }) =>
+      this.setState({
+        cameraPermission: status === "granted"
+      })
+    );
+  }
+
   render() {
+    const { cameraPermission } = this.state;
+
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        {cameraPermission === null ? (
+          <Text>Waiting for permission...</Text>
+        ) : cameraPermission === false ? (
+          <Text>Permission denied</Text>
+        ) : (
+          <Autoshoot />
+        )}
+      </View>
+    );
+  }
+}
+
+class Autoshoot extends React.Component {
+  state = {
+    photo: null
+  };
+
+  takePicture = () => {
+    this.camera
+      .takePictureAsync({
+        quality: 0.1,
+        base64: true,
+        exif: false
+      })
+      .then(photo => {
+        this.setState({ photo });
+      });
+  };
+
+  clearPhoto = () => {
+    this.setState({photo: null});
+  }
+
+  render() {
+    const { photo } = this.state;
+    return (
+      <View style={{ flex: 1, width: "100%" }}>
+        {photo ? (
+          <ImageBackground style={{ flex: 1 }} source={{ uri: photo.uri }}>
+            <TouchableOpacity style={{ flex: 1 }} onPress={this.clearPhoto} />
+          </ImageBackground>
+        ) : (
+          <Camera
+            style={{ flex: 1 }}
+            type={Camera.Constants.Type.back}
+            ref={cam => (this.camera = cam)}
+          >
+            <TouchableOpacity style={{ flex: 1 }} onPress={this.takePicture} />
+          </Camera>
+        )}
       </View>
     );
   }
@@ -14,8 +84,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
